fdf0ee16bbf849cafc9db6ae24aa63611d2144ee
UselessHumster <79360700+UselessHumster@users.noreply.github.com>
2025-07-09 16:47:18 +0300

    Added logging to the active_directory actions

--- unlock_bot/active_directory/backend.py
+++ unlock_bot/active_directory/backend.py
@@ -3,27 +3,38 @@ from unlock_bot.config import settings
 from pyad.pyadexceptions import invalidResults
 from pyad import adsearch, aduser
 import subprocess as sp
+import logging
 
 
 def get_cn_of_ad_user(upn):
-    return adsearch.by_upn(upn)
+    logging.info(f'Searching cn by {upn=}')
+    cn = adsearch.by_upn(upn)
+    logging.info(f'Found {cn=}')
+    return cn
 
 
 def is_ad_user_exists(upn) -> bool:
     try:
+        logging.info(f'Checking if user exist by {upn=}')
         get_cn_of_ad_user(upn)
+        logging.info(f'User {upn=} exists')
         return True
 
     except invalidResults:
+        logging.info(f'User {upn=} does not exists')
         return False
 
 def get_ad_user_by_upn(upn):
     if f'@{settings.DOMAIN}' not in upn:
         upn += f'@{settings.DOMAIN}'
 
+    logging.info(f'Getting user by {upn=}')
+
     if is_ad_user_exists(upn):
         cn = get_cn_of_ad_user(upn)
-        return aduser.ADUser.from_dn(cn)
+        ad_user = aduser.ADUser.from_dn(cn)
+        logging.info(f'Found {ad_user=}')
+        return ad_user
 
     return None
 
