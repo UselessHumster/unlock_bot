b6c1069a3c715515f79fe038f8665837cd03198a
UselessHumster <79360700+UselessHumster@users.noreply.github.com>
2025-07-10 14:38:37 +0300

    Renaming active_directory to ad

--- unlock_bot/active_directory/__init__.py
+++
@@ -1,6 +0,0 @@
-from unlock_bot.active_directory.backend import (is_ad_user_exists,
-                                                 get_ad_user_by_upn, get_locked_users_list)
-
-__all__ = ['get_ad_user_by_upn',
-           'is_ad_user_exists',
-           'get_locked_users_list']
---
+++ unlock_bot/ad/__init__.py
@@ -0,0 +1,6 @@
+from unlock_bot.ad.backend import (is_ad_user_exists,
+                                   get_ad_user_by_upn, get_locked_users_list)
+
+__all__ = ['get_ad_user_by_upn',
+           'is_ad_user_exists',
+           'get_locked_users_list']
