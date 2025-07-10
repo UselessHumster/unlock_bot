e7770ca96eb4cbda2538c7acfdfa9aea265e6581
UselessHumster <79360700+UselessHumster@users.noreply.github.com>
2025-07-10 11:57:29 +0300

    Added correct dates on logs files, also switched mode to append

--- unlock_bot/scripts/start.py
+++ unlock_bot/scripts/start.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unlock_bot.telegram import reg_handlers, reg_commands
 from unlock_bot.config.aiogram_collection import Dispatcher, MemoryStorage, executor
 from unlock_bot.config import bot
@@ -8,9 +9,12 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 def start():
+    today = datetime.now().__format__("%Y-%m-%d")
     reg_commands(dp)
     reg_handlers(dp)
-    logging.basicConfig(level=logging.INFO, filename='Logs/unlock_bot.log', filemode='w',
+    logging.basicConfig(level=logging.INFO,
+                        filename=f'Logs/unlock_bot_{today}.log',
+                        filemode='a',
                         format='[%(asctime)s]:%(levelname)s:\t%(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
     executor.start_polling(dp, skip_updates=True)
